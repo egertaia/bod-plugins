d5a1eff741bfb485d99cf796912997e72d4a33a4
Egert Aia <egert.aia@softwerk.ee>
2021-03-10 20:05:11 +0200

    Went over to use TimeoutUntil

--- bodvonblessed/src/main/java/net/runelite/client/plugins/bodvonblessed/tasks/DropFishTask.java
+++ bodvonblessed/src/main/java/net/runelite/client/plugins/bodvonblessed/tasks/DropFishTask.java
@@ -2,10 +2,13 @@ package net.runelite.client.plugins.bodvonblessed.tasks;
 
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.AnimationID;
+import net.runelite.api.ItemID;
 import net.runelite.api.events.GameTick;
 import net.runelite.client.plugins.bodvonblessed.BodVonBlessedPlugin;
 import net.runelite.client.plugins.bodvonblessed.Task;
 import net.runelite.client.plugins.bodutils.InventoryUtils;
+import net.runelite.client.plugins.bodvonblessed.TimeoutUntil;
 
 @Slf4j
 public class DropFishTask extends Task
@@ -23,7 +26,12 @@ public class DropFishTask extends Task
 	public void onGameTick(GameTick event)
 	{
 		inventory.dropItems(rawFishIds, true, config.sleepMin(), config.sleepMax());
-		BodVonBlessedPlugin.timeout = tickDelay();
+
+		BodVonBlessedPlugin.conditionTimeout = new TimeoutUntil(
+			() -> !inventory.containsItem(rawFishIds),
+			() -> inventory.containsItem(rawFishIds),
+			2 + tickDelay()
+		);
 	}
 
 	@Override
